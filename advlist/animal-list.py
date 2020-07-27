0f2f97c312419dfc900ccf134a8a855f6d324cb8
Jack Stephens <JacksonDStephens@gmail.com>
2020-07-27 18:36:44 -0400

    LISTSgit add *!

---
+++ advlist/animal-list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+animal_list = ["Fox", "Dog", "Cat", "Bee", "Ant"]
+
+for x in animal_list:
+    print(x, end=" ")
+
---
+++ advlist/complex01.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+list1 = ["cisco_nxos", "arista_eos", "cisco_ios"]
+print(list1)
+
+
+
+
